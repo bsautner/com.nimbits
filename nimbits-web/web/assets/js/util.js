f810c12ec903072063c4a808cf34b4d6c8c9a25e
Benjamin Sautner <bsautner@gmail.com>
2012-03-20 17:00:26 -0400

    fixed index

--- nimbits-web/web/assets/js/util.js
+++ nimbits-web/web/assets/js/util.js
@@ -1,16 +1,3 @@
-/*
- * Copyright (c) 2010 Tonic Solutions LLC.
- *
- * http://www.nimbits.com
- *
- *
- * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/gpl.html
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the license is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, eitherexpress or implied. See the License for the specific language governing permissions and limitations under the License.
- */
-
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-11739682-12']);
 _gaq.push(['_trackPageview']);
@@ -116,13 +103,17 @@ function getHTTPObject() {
     return xhr;
 }
 function getContent(page) {
+   try {
+       _gaq.push(['_trackEvent', page, 'load'])
+       request = getHTTPObject();
+       request.onreadystatechange = sendData;
+       request.open("POST", page, true);
+        request.send(null);
+   }
+   catch (ex) {
+       alert(ex);
+   }
 
-    if (! page.sets)
-    _gaq.push(['_trackEvent', page, 'load'])
-    request = getHTTPObject();
-    request.onreadystatechange = sendData;
-    request.open("POST", page, true);
-    request.send(null);
 
 }
 
@@ -133,7 +124,7 @@ function sendData() {
         prettyPrint();
     }
     else if (request.readyState == 1) {
-       dC.innerHTML = "Requesting content..."
+        dC.innerHTML = "Requesting content..."
     }
 }
 

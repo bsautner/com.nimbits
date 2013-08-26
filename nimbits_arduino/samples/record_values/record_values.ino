29d17d6034dd1e6a39c917206a5a5ad53451a750
Benjamin Sautner <bsautner@gmail.com>
2013-08-26 17:27:21 -0400

    dht sensor sample

--- nimbits_arduino/samples/record_values/record_values.ino
+++ nimbits_arduino/samples/record_values/record_values.ino
@@ -14,27 +14,15 @@
 #include "Nimbits.h"
 #include <Client.h>
 #include <Ethernet.h>
-
 #include <SPI.h>
 #include <PString.h>
 #include <stdlib.h>
-
-#define MAX_KEY_SIZE 64
-#define CATEGORY 2
-#define POINT 1
-#define SUBSCRIPTION 5
- 
  
 //nimbits settings, set the instance name (nimbits-02 is the public cloud on https://cloud.nimbits.com) the email of the account owner, and a read write key they have created.
 String instance = "nimbits-02";
 char owner[] = "bsautner@gmail.com";
 String readWriteKey = "key";
-long randNumber;
 byte mac[] = {0x90, 0xA2, 0xDA, 0x00, 0x54, 0x39}; //this ethernet shield's MAC address
-
-//the names of the entities that will either be created or queried on startup
-char folder[] = "RD";
-char point[] = "RD Alerting Point";
 
 Nimbits nimbits(instance, owner, readWriteKey);
 

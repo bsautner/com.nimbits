631838ccbfb6d057ad1dfd3c92f3ce0bb5480afb
benjamin sautner <bsautne@gmail.com>
2014-04-23 18:47:33 -0400

    moved android http stuff to io lib

--- nimbits_io/src/main/java/com/nimbits/http/HttpHelper.java
+++ nimbits_io/src/main/java/com/nimbits/http/HttpHelper.java
@@ -1,112 +1,72 @@
+/*
+ * Copyright (c) 2013 Nimbits Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.  See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package com.nimbits.http;
 
-import com.nimbits.client.enums.Parameters;
-import com.nimbits.client.enums.SettingType;
-import org.apache.http.*;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.params.ClientPNames;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.CoreProtocolPNames;
-import org.apache.http.params.HttpParams;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.lang.reflect.Type;
 import java.util.List;
 
+/**
+ * Author: Benjamin Sautner
+ * Date: 1/18/13
+ * Time: 7:01 AM
+ */
+@SuppressWarnings({"unchecked", "unused"})
 public class HttpHelper {
 
+    private final String email;
+    private final String apiKey;
+
+    public HttpHelper(String email, String apiKey) {
+        this.email = email;
+        this.apiKey = apiKey;
+    }
 
 
-    public static String doGet(String email, String base, String path, List<BasicNameValuePair> parameters) throws IOException {
 
-        DefaultHttpClient httpClient;
+    public <T, K> List<T> doGet(final Class<K> clz,
+                                       final UrlContainer postUrl,
+                                       final List<BasicNameValuePair> parameters,
+                                       final Type type,
+                                       final boolean expectList
 
+    ) {
+        List<T> result;
 
-        parameters.add((new BasicNameValuePair(Parameters.email.getText(), email)));
-        String u = base + path + "?";
-        for (BasicNameValuePair p : parameters) {
-            u += p.getName() + "=" + p.getValue() + "&";
-
-        }
-        u = u.substring(0, u.length()-1);
-        HttpParams headerParams = new BasicHttpParams();
-        headerParams.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
-        headerParams.setBooleanParameter(ClientPNames.HANDLE_REDIRECTS, true);
-        HttpGet http = new HttpGet(u);
-
-        httpClient = new DefaultHttpClient(headerParams);
-        http.setHeader(SettingType.apiKey.getName(), SettingType.apiKey.getDefaultValue());
+        result = doHttpGet(clz, postUrl, parameters, type, expectList);
 
 
-        HttpResponse response = httpClient.execute(http);
-        if (response.getStatusLine().getStatusCode() != 200) {
-            for (Header header : response.getAllHeaders()) {
-                System.out.println(header.getName() + "=" + header.getValue());
-            }
-            throw new RuntimeException("server returned an error: " + response.getStatusLine().getReasonPhrase());
-
-        }
-        HttpEntity entity = response.getEntity();
-        InputStream inputStream = entity.getContent();
-        return streamToString(inputStream);
+        return result;
 
 
     }
 
-    public static String streamToString(InputStream is) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        return sb.toString();
+    private <T, K> List<T> doHttpGet(Class<K> clz, UrlContainer postUrl, List<BasicNameValuePair> parameters, Type type, boolean expectList) {
+        List<T> response = new HttpTransaction(email, apiKey).doGet(clz, postUrl, parameters, type, expectList);
+        return response;
     }
 
-    public static String doPost(String email, String base, String path, List<BasicNameValuePair> parameters) throws IOException {
+    public <T, K>  List<T> doPost(final Class<K> clz,
+                                         final UrlContainer postUrl,
+                                         final List<BasicNameValuePair> parameters,
+                                         final Type type,
+                                         final boolean expectList) {
 
-        DefaultHttpClient httpClient;
-
-        HttpPost httppost = new HttpPost(base + path);
-        parameters.add((new BasicNameValuePair(Parameters.email.getText(), email)));
-//        if (key != null) {
-//            parameters.add((new BasicNameValuePair(Parameters.key.getText(), key.getCode())));
-//        }
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(parameters.size());
-        for (BasicNameValuePair value : parameters) {
-            nameValuePairs.add(value);
-        }
-        HttpParams headerParams = new BasicHttpParams();
-        headerParams.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
-        headerParams.setBooleanParameter(ClientPNames.HANDLE_REDIRECTS, true);
-        headerParams.setParameter(SettingType.apiKey.getName(), SettingType.apiKey.getDefaultValue());
-
-        httpClient = new DefaultHttpClient(headerParams);
-//        if (userApiKey) {
-        httppost.setHeader(SettingType.apiKey.getName(), SettingType.apiKey.getDefaultValue());
-//        }
-        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-
-        HttpResponse response = httpClient.execute(httppost);
-        if (response.getStatusLine().getStatusCode() != 200 && response.getStatusLine().getStatusCode() != 204) {
-            for (Header header : response.getAllHeaders()) {
-                System.out.println(header.getName() + "=" + header.getValue());
-            }
-            throw new RuntimeException("server returned an error: " + response.getStatusLine().getReasonPhrase());
-        }
-        HttpEntity entity = response.getEntity();
-        InputStream inputStream = entity.getContent();
-
-        return streamToString(inputStream);
-
-
+        return new HttpTransaction(email, apiKey).doPost(clz, postUrl, parameters, type, expectList);
     }
+
+
+
+
 }
---
+++ nimbits_io/src/main/java/com/nimbits/http/Transaction.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2013 Nimbits Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.  See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.nimbits.http;
+
+import com.google.common.collect.Range;
+import com.nimbits.client.android.AndroidControl;
+import com.nimbits.client.enums.EntityType;
+import com.nimbits.client.model.entity.Entity;
+import com.nimbits.client.model.simple.SimpleValue;
+import com.nimbits.client.model.user.User;
+import com.nimbits.client.model.value.Value;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by benjamin on 10/20/13.
+ */
+public interface Transaction {
+
+
+    List<User> getSession();
+
+    List<User> getSession(String email, String key);
+
+    List<Value> getValue(Entity entity);
+
+    <T> List<T> getTree();
+
+    List<Value> postValue(Entity entity, Value value);
+
+    List<Value> getSeries(String entity);
+
+    List<Value> getSeries(String entity, Range<Date> range);
+
+    void deleteEntity(Entity entity);
+
+    <T, K> List<T> addEntity(Entity entity, Class<K> clz);
+
+    <T> List<T> updateEntity(Entity entity, Class<T> clz);
+
+    <T, K> List<T> getEntity(SimpleValue<String> entityId, EntityType type, Class<K> clz);
+
+    List<AndroidControl> getControl();
+
+    void doHeartbeat(Entity parent);
+}

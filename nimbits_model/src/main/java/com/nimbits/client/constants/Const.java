67aed550d862a65689097f994e95f4b5159cff42
Benjamin Sautner <bsautner@gmail.com>
2015-07-04 11:29:08 -0400

    3.9.8

--- nimbits_model/src/main/java/com/nimbits/client/constants/Const.java
+++ nimbits_model/src/main/java/com/nimbits/client/constants/Const.java
@@ -15,7 +15,7 @@ package com.nimbits.client.constants;
 
 public class Const {
 
-    public static final String VERSION = "3.9.7";
+    public static final String VERSION = "3.9.8";
 
     public static final String SOCKET_RELAY = "130.211.147.107:8080";
 

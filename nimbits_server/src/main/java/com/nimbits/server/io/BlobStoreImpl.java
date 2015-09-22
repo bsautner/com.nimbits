b6e5f8927389d0c3164d0df2b24909c3e9bb14c8
Benjamin Sautner <bsautner@gmail.com>
2015-09-22 13:03:14 -0400

    fix snapshots

--- nimbits_server/src/main/java/com/nimbits/server/io/BlobStoreImpl.java
+++ nimbits_server/src/main/java/com/nimbits/server/io/BlobStoreImpl.java
@@ -186,7 +186,7 @@ public class BlobStoreImpl implements BlobStore {
             List<Value> values = readValuesFromFile(root + "/" + entity.getKey() + "/" + SNAPSHOT);
 
             if (values.isEmpty()) {
-                value = new Value.Builder().doubleValue(0.0).timestamp(new Date()).create();
+                value = new Value.Builder().doubleValue(0.0).timestamp(new Date(0)).create();
                 createSnapshot(entity, value);
             } else {
                 value = values.get(0);

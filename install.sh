8254f718b77c222a737e2e6b17fdaa426f08acf8
Benjamin Sautner <bsautner@gmail.com>
2016-08-12 14:53:00 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt-get update
+apt-get upgrade -y
+apt-get install maven -y
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+apt-get install oracle-java8-installer
+apt-get install tomcat7
+rm -fR /var/lib/tomcat7/webapps/ROOT
+mvn clean package
+cp ./nimbits_server/target/nimbits_server.war /var/lib/tomcat7/webapps/ROOT.war -v
+service tomcat7 restart

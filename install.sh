fc7f79d201f9655d1f6fd72daab459cbeb726514
Benjamin Sautner <bsautner@gmail.com>
2016-08-12 15:45:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt-get upgrade -y
 apt-get install maven -y
 add-apt-repository ppa:webupd8team/java -y
 apt-get update
-apt-get install oracle-java8-installer
+apt-get install oracle-java8-installer -y
 apt-get install tomcat7
 rm -fR /var/lib/tomcat7/webapps/ROOT
 mvn clean package

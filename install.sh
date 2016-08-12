65e5222a6dce754e5fe837c1e551938f8f430701
Benjamin Sautner <bsautner@gmail.com>
2016-08-12 16:28:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get upgrade -y
-apt-get install maven -y
+
 add-apt-repository ppa:webupd8team/java -y
 apt-get update
 apt-get install oracle-java8-installer -y
@@ -9,3 +9,10 @@ rm -fR /var/lib/tomcat7/webapps/ROOT
 mvn clean package
 cp ./nimbits_server/target/nimbits_server.war /var/lib/tomcat7/webapps/ROOT.war -v
 service tomcat7 restart
+
+#set java home for tomcat
+#http://askubuntu.com/questions/154953/specify-jdk-for-tomcat7
+#update-alternatives --display java
+#sudo vi /etc/default/tomcat7
+
+apt-get install git maven -y

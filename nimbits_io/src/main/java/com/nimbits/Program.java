5d1bc91bd79b5661193812d9eb92d58a6542ae56
Benjamin Sautner <bsautner@gmail.com>
2016-09-28 15:56:18 -0400

    added console output and maven assembly plugin

---
+++ nimbits_io/src/main/java/com/nimbits/Program.java
@@ -0,0 +1,12 @@
+package com.nimbits;
+
+import com.nimbits.client.constants.Const;
+
+public class Program {
+
+    public static void main(String... args) {
+
+        System.out.println("Hello Nimbits " + Const.VERSION);
+
+    }
+}

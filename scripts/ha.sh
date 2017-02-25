c08eeae3dbc34e89d2c4bf6998f23745368d3245
Benjamin Sautner <bsautner@gmail.com>
2017-02-25 09:10:23 -0500

    upgrade script

---
+++ scripts/ha.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get upgrade -y
+
+service tomcat8 stop
+cd ..
+git fetch
+git reset --hard origin/ha
+chmod +x *.sh
+mvn clean install
+cp ./nimbits_server/target/nimbits_server.war /opt/tomcat/webapps/nimbits.war
+service tomcat8 start
+
+
+

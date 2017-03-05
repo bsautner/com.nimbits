2944e152186c048199e2fbd6c656c19dea958256
Benjamin Sautner <bsautner@gmail.com>
2017-03-05 10:30:15 -0500

    upgrade scripts

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -3,17 +3,13 @@ apt-get update
 apt-get upgrade -y
 
 service tomcat8 stop
+cp /opt/tomcat/webapps/nimbits/WEB-INF/classes/application.properties ~/application.properties.old
 cd ..
-git fetch
-git branch -D nimbits_update_tmp_branch
-git checkout -b nimbits_update_tmp_branch
-git branch -D master
-git checkout -b master origin/master
-mvn clean install
-cp -v /opt/tomcat/webapps/nimbits/WEB-INF/applicationContext.xml /tmp/applicationContext.xml.backup
+vn clean install
 cp ./nimbits_server/target/nimbits_server.war /opt/tomcat/webapps/nimbits.war
 service tomcat8 start
-cp -fv /tmp/applicationContext.xml.backup /opt/tomcat/webapps/nimbits/WEB-INF/applicationContext.xml
+cp -f  ~/application.properties.old /opt/tomcat/webapps/nimbits/WEB-INF/classes/application.properties
+rm -f  ~/application.properties.old
 service tomcat8 restart
 
 

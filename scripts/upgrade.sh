4558e481248ed7f0d4da384df1da4dfb54bc49fd
Benjamin Sautner <bsautner@gmail.com>
2017-03-25 17:07:29 -0400

    fix in commit

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,11 +4,11 @@ apt-get upgrade -y
 
 service tomcat8 stop
 cp -fv /opt/tomcat/webapps/nimbits/WEB-INF/classes/application.properties ~/application.properties.old
-mvn -f ../pom.xml clean install
+mvn -f ../pom.xml clean package
 cp ../nimbits_server/target/nimbits_server.war /opt/tomcat/webapps/nimbits.war
 service tomcat8 start
 echo "waiting for tomcat so explode the war"
-sleep 10s
+sleep 20s
 cp -fv  ~/application.properties.old /opt/tomcat/webapps/nimbits/WEB-INF/classes/application.properties
 rm -fv  ~/application.properties.old
 service tomcat8 restart

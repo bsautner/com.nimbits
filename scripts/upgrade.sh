3b81b4094f21f6926613f1fd5f89969a4a80eaf3
Benjamin Sautner <bsautner@gmail.com>
2017-03-26 11:55:10 -0400

    fix for update script

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+export version=4.1.1
 apt-get update
 apt-get upgrade -y
 
 service tomcat8 stop
 cp -fv /opt/tomcat/webapps/nimbits/WEB-INF/classes/application.properties ~/application.properties.old
+mvn install:install-file -Dfile=./nimbits_server/src/main/resources/nimbits_core-${version}.out.jar -DgroupId=com.nimbits -DartifactId=nimbits_core -Dversion=${version} -Dpackaging=jar
+
 mvn -f ../pom.xml clean package
 cp ../nimbits_server/target/nimbits_server.war /opt/tomcat/webapps/nimbits.war
 service tomcat8 start

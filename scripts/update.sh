788c30205bb75b22a9342998af650aa037f89881
Benjamin Sautner <bsautner@gmail.com>
2017-04-03 12:23:28 -0400

    4.0.3

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 #copy this file out of the source dir to ~ to update a system regularly
-export version=4.1.2
+export version=4.1.3
 cd com.nimbits
 git fetch
 git reset --hard origin/master
-#mvn install:install-file -Dfile=com.nimbits/nimbits_server/src/main/resources/nimbits_core-${version}.out.jar -DgroupId=com.nimbits -DartifactId=nimbits_core -Dversion=${version} -Dpackaging=jar
-
 cd scripts
 chmod +x *.sh
 
